d3e482de2e5f1cff821562bd2e839891a1731315
Wei Yu <weiyu@yslogin1.(none)>
2017-05-19 12:17:08 -0600

    Update the setEnvar.sh for compiling on WCOSS machine.

--- trunk/NDHMS/setEnvar.sh
+++ trunk/NDHMS/setEnvar.sh
@@ -7,7 +7,7 @@
 export WRF_HYDRO=1
 
 ### turn on output information during running time.
-export HYDRO_D=1
+export HYDRO_D=0
 
 ### turn on distributed parameters for NoahMP
 export SPATIAL_SOIL=1  
@@ -25,4 +25,4 @@ export HYDRO_REALTIME=1
 export NCEP_WCOSS=1
 
 ### turn nudging off
-export WRF_HYDRO_NUDGING=0
+export WRF_HYDRO_NUDGING=1
