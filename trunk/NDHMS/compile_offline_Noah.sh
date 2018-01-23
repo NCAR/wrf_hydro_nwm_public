eb6053d2c4b3c37f6759a0d3e49873bd9660e96a
Katelyn FitzGerald <katelynw@ucar.edu>
2018-01-23 09:05:29 -0700

    Modify Noah compile script to copy over appropriate namelists and parameter TBLs

--- trunk/NDHMS/compile_offline_Noah.sh
+++ trunk/NDHMS/compile_offline_Noah.sh
@@ -7,10 +7,14 @@ cp arc/Makefile.Noah Makefile
 ln -sf CPL/Noah_cpl LandModel_cpl
 ln -sf Land_models/Noah LandModel
 cat macros LandModel/user_build_options.bak  > LandModel/user_build_options
-make clean ; rm -f Run/wrf_hydro_Noah.exe
+make clean ; rm -f Run/wrf_hydro_Noah.exe ; rm -f Run/*TBL ; rm -f Run/*namelist*
 
 
 cat macros LandModel/user_build_options.bak > LandModel/user_build_options
 make
 cd Run
 mv wrf_hydro.exe wrf_hydro_Noah.exe ; ln -sf wrf_hydro_Noah.exe wrf_hydro.exe
+cp ../Land_models/Noah/Run/*TBL .
+cp ../template/Noah/namelist.hrldas .
+cp ../template/HYDRO/HYDRO.TBL .
+cp ../template/HYDRO/hydro.namelist .
