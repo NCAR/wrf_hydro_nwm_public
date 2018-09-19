a8785c3e3b05145f6e12449a8bfc26270a6f65ca
jmills-ncar <jmills@ucar.edu>
2018-09-19 10:15:36 -0600

    fixed paths in examples

--- tests/local/examples/ex1_run_croton_cheyenne.sh
+++ tests/local/examples/ex1_run_croton_cheyenne.sh
@@ -2,8 +2,8 @@
 
 # These paths point to a preconfigured conda environment. Likewise, you can install your own
 # using the utils/nwm_testing.yml conda environment file.
-export PATH="/glade/p/work/jmills/nwm_testing/miniconda3/envs/nwm_data_pipeline/bin:$PATH"
-export PYTHONPATH="/glade/p/work/jmills/nwm_testing/miniconda3/envs/nwm_data_pipeline/lib/python3.6/site-packages/"
+export PATH="/glade/work/jmills/nwm_testing/miniconda3/envs/nwm_testing/bin:$PATH"
+export PYTHONPATH="/glade/work/jmills/nwm_testing/miniconda3/envs/nwm_testing/lib/python3.7/site-packages"
 
 # Run the tests interactively, no scheduler
 python /glade/scratch/jmills/wrf_hydro_nwm_public/tests/local/run_tests.py \
