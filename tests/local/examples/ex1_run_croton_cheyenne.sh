eba4a760882db9c37309b5a7254387a704c888ff
jmills-ncar <jmills@ucar.edu>
2018-09-19 10:18:48 -0600

    changed hardcoded user to $USER

--- tests/local/examples/ex1_run_croton_cheyenne.sh
+++ tests/local/examples/ex1_run_croton_cheyenne.sh
@@ -6,11 +6,11 @@ export PATH="/glade/work/jmills/nwm_testing/miniconda3/envs/nwm_testing/bin:$PAT
 export PYTHONPATH="/glade/work/jmills/nwm_testing/miniconda3/envs/nwm_testing/lib/python3.7/site-packages"
 
 # Run the tests interactively, no scheduler
-python /glade/scratch/jmills/wrf_hydro_nwm_public/tests/local/run_tests.py \
+python /glade/scratch/$USER/wrf_hydro_nwm_public/tests/local/run_tests.py \
 --config nwm_ana nwm_long_range gridded reach \
 --compiler ifort \
---output_dir /glade/scratch/jmills/test_out \
---candidate_dir /glade/scratch/jmills/wrf_hydro_nwm_public/ \
---reference_dir /glade/scratch/jmills/wrf_hydro_nwm_public/ \
+--output_dir /glade/scratch/$USER/test_out \
+--candidate_dir /glade/scratch/$USER/wrf_hydro_nwm_public/ \
+--reference_dir /glade/scratch/$USER/wrf_hydro_nwm_public/ \
 --domain_tag dev \
 --ncores 3
