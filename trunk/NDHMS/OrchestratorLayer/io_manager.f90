c842d3c15404ca3096248e4f418a27236bda5ff9
Alessandro Fanfarillo <elfanfa@ucar.edu>
2019-08-29 16:40:46 +0000

    Add orchestrator, config and io files into the build process

---
+++ trunk/NDHMS/OrchestratorLayer/io_manager.f90
@@ -0,0 +1,32 @@
+module io_manager_base
+  use netcdf_layer_base
+  implicit none
+  
+  type :: IOManager_
+     logical :: parallel = .false.
+     class(NetCDF_layer_),allocatable :: netcdf_layer
+   contains
+  end type IOManager_
+
+  interface IOManager_
+     module procedure IOManager_init
+  end interface IOManager_
+    
+contains
+
+  type(IOManager_) function IOManager_init(parallel)
+    implicit none
+
+    logical, optional, intent(in) :: parallel
+
+    if(.not.present(parallel) .or. (present(parallel) .and. parallel .eqv. .false.)) then
+       IOManager_init%parallel = .false.
+       allocate(NetCDF_serial_ :: IOManager_init%netcdf_layer)
+    else
+       IOManager_init%parallel = .true.
+       allocate(NetCDF_parallel_ :: IOManager_init%netcdf_layer)
+    end if
+    
+  end function IOManager_init
+  
+end module io_manager_base
