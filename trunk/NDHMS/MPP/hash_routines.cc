9d4ade7cf6793e42df822b2b0fcae2c83afb40c0
afanfa <elfanfa@ucar.edu>
2020-04-27 09:18:54 -0600

    Making hash Fortran compliant

--- trunk/NDHMS/MPP/hash_routines.cc
+++ trunk/NDHMS/MPP/hash_routines.cc
@@ -8,14 +8,14 @@ void getLocalIndx_C(int * gLinkId, int *llinkid, int * llinkidindx, int glinksl,
   map<int,int>::iterator it;
 
   for(int i=0; i < llinklen; i++)
-    hash.insert(std::pair<int,int>(llinkid[i],i));
+    hash.insert(std::pair<int,int>(llinkid[i],i+1));
 
   for(int i=0; i < glinksl; i++)
     {
       it = hash.find(gLinkId[i]);
       if(it != hash.end())
 	{
-	  llinkidindx[it->second] = i;
+	  llinkidindx[it->second] = i+1;
 	  hash.erase(it);
 	}
     }
