342f116af386ab0e6e2374b7b140269f498d030e
BeheenTrimble <trimbleava@gmail.com>
2020-06-06 18:44:18 +0000

    effor to test nwm in coupled mode

---
+++ trunk/NDHMS/CPL/NUOPC_cpl/TEST/ATM.F90
@@ -0,0 +1,335 @@
+!==============================================================================
+! Earth System Modeling Framework
+! Copyright 2002-2019, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!==============================================================================
+
+module ATM
+
+  !-----------------------------------------------------------------------------
+  ! ATM Component.
+  !-----------------------------------------------------------------------------
+
+  use ESMF
+  use NUOPC
+  use NUOPC_Model, &
+    model_routine_SS    => SetServices, &
+    model_label_Advance => label_Advance
+  
+  implicit none
+  
+  private
+  
+  public SetServices
+  
+  !-----------------------------------------------------------------------------
+  contains
+  !-----------------------------------------------------------------------------
+  
+  subroutine SetServices(model, rc)
+    type(ESMF_GridComp)  :: model
+    integer, intent(out) :: rc
+    
+    rc = ESMF_SUCCESS
+    
+    ! the NUOPC model component will register the generic methods
+    call NUOPC_CompDerive(model, model_routine_SS, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    
+    ! set entry point for methods that require specific implementation
+    call NUOPC_CompSetEntryPoint(model, ESMF_METHOD_INITIALIZE, &
+      phaseLabelList=(/"IPDv00p1"/), userRoutine=InitializeP1, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompSetEntryPoint(model, ESMF_METHOD_INITIALIZE, &
+      phaseLabelList=(/"IPDv00p2"/), userRoutine=InitializeP2, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    
+    ! attach specializing method(s)
+    call NUOPC_CompSpecialize(model, specLabel=model_label_Advance, &
+      specRoutine=ModelAdvance, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    
+  end subroutine
+  
+  !-----------------------------------------------------------------------------
+
+  subroutine InitializeP1(model, importState, exportState, clock, rc)
+    type(ESMF_GridComp)  :: model
+    type(ESMF_State)     :: importState, exportState
+    type(ESMF_Clock)     :: clock
+    integer, intent(out) :: rc
+    
+    rc = ESMF_SUCCESS
+    
+    ! Disabling the following macro, e.g. renaming to WITHIMPORTFIELDS_disable,
+    ! will result in a model component that does not advertise any importable
+    ! Fields. Use this if you want to drive the model independently.
+#define WITHIMPORTFIELDS
+#ifdef WITHIMPORTFIELDS
+    ! importable field: sea_surface_temperature
+    call NUOPC_Advertise(importState, &
+      StandardName="sea_surface_temperature", name="sst", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+#endif
+    
+    ! exportable field: air_pressure_at_sea_level
+    call NUOPC_Advertise(exportState, &
+      StandardName="streamflow", name="pmsl", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    
+    ! exportable field: surface_net_downward_shortwave_flux
+    call NUOPC_Advertise(exportState, &
+      StandardName="surface_net_downward_shortwave_flux", name="rsns", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+  
+  !-----------------------------------------------------------------------------
+
+  subroutine InitializeP2(model, importState, exportState, clock, rc)
+    type(ESMF_GridComp)  :: model
+    type(ESMF_State)     :: importState, exportState
+    type(ESMF_Clock)     :: clock
+    integer, intent(out) :: rc
+    
+    ! local variables    
+    type(ESMF_VM)        :: vm
+    integer              :: localPet
+    integer              :: petCount
+    integer,parameter    :: gblElementCnt = 111
+    integer              :: gblElementDiv
+    integer              :: gblElementExt
+    integer              :: locElementBeg
+    integer              :: locElementCnt
+    integer,allocatable  :: arbSeqIndexList(:)
+    integer              :: i
+    type(ESMF_DistGrid)  :: distgrid
+    type(ESMF_Field)     :: field
+    type(ESMF_LocStream) :: locStreamIn
+    type(ESMF_LocStream) :: locStreamOut
+    
+    rc = ESMF_SUCCESS
+
+    call ESMF_VMGetCurrent(vm=vm, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_VMGet(vm=vm, localPet=localPet, petCount=petCount, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! calculate local element count and first local element
+    gblElementDiv = gblElementCnt/petCount
+    gblElementExt = MOD(gblElementCnt,petCount)
+    if (localPet.eq.(petCount-1)) then
+      locElementCnt = gblElementDiv + gblElementExt
+    else
+      locElementCnt = gblElementDiv
+    endif
+    locElementBeg = 1 + (gblElementDiv*localPet)
+
+    ! create local element list
+    allocate(arbSeqIndexList(locElementCnt))
+    do i=1, locElementCnt
+      arbSeqIndexList(i) = locElementBeg + (i - 1)
+    enddo
+
+    print *,"ATM: ",localPet,"arbSeqIndices=", &
+      arbSeqIndexList(1),arbSeqIndexList(locElementCnt)
+
+    ! create DistGrid
+    distgrid = ESMF_DistGridCreate(minIndex=(/1/), &
+      maxIndex=(/gblElementCnt/), rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+!    ! create DistGrid
+!    distgrid = ESMF_DistGridCreate(arbSeqIndexList=arbSeqIndexList, rc=rc)
+!    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+!      line=__LINE__, &
+!      file=__FILE__)) &
+!      return  ! bail out
+
+    deallocate(arbSeqIndexList)
+
+    ! create a LocationObject object for Fields
+    locStreamIn = ESMF_LocStreamCreate(distgrid=distgrid, &
+      coordSys=ESMF_COORDSYS_CART, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    locStreamOut = locStreamIn ! for now out same as in
+    
+#ifdef WITHIMPORTFIELDS
+    ! importable field: sea_surface_temperature
+    field = ESMF_FieldCreate(name="sst", locStream=locStreamIn, &
+      typekind=ESMF_TYPEKIND_R8, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_Realize(importState, field=field, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+#endif
+
+    ! exportable field: air_pressure_at_sea_level
+#ifdef CREATE_AND_REALIZE
+    ! This branch shows the standard procedure of creating a complete field
+    ! with Grid and memory allocation, and then calling Realize() for it.
+    field = ESMF_FieldCreate(name="pmsl", locStream=locStreamOut, &
+      typekind=ESMF_TYPEKIND_R8, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_Realize(exportState, field=field, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+#else
+    ! This branch shows the alternative way of "realizing" an advertised field.
+    ! It accesses the empty field that was created during advertise, and
+    ! finishes it, setting a Grid on it, and then calling FieldEmptyComplete().
+    ! No formal Realize() is then needed.
+    call ESMF_StateGet(exportState, field=field, itemName="pmsl", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call ESMF_FieldEmptySet(field, locStream=locStreamOut, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call ESMF_FieldEmptyComplete(field, typekind=ESMF_TYPEKIND_R8, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+#define WITH_FORMAL_REALIZE
+#ifdef WITH_FORMAL_REALIZE
+    ! There is not need to formally call Realize() when completing the 
+    ! adverised field directly. However, calling Realize() also works.
+    call NUOPC_Realize(exportState, field=field, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+#endif
+#endif
+
+    ! exportable field: surface_net_downward_shortwave_flux
+    field = ESMF_FieldCreate(name="rsns", locStream=locStreamOut, &
+      typekind=ESMF_TYPEKIND_R8, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_Realize(exportState, field=field, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+  
+  !-----------------------------------------------------------------------------
+
+  subroutine ModelAdvance(model, rc)
+    type(ESMF_GridComp)  :: model
+    integer, intent(out) :: rc
+    
+    ! local variables
+    type(ESMF_Clock)            :: clock
+    type(ESMF_State)            :: importState, exportState
+    character(len=160)          :: msgString
+
+#define NUOPC_TRACE__OFF
+#ifdef NUOPC_TRACE
+    call ESMF_TraceRegionEnter("ATM:ModelAdvance")
+#endif
+
+    rc = ESMF_SUCCESS
+    
+    ! query the Component for its clock, importState and exportState
+    call NUOPC_ModelGet(model, modelClock=clock, importState=importState, &
+      exportState=exportState, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! HERE THE MODEL ADVANCES: currTime -> currTime + timeStep
+    
+    ! Because of the way that the internal Clock was set by default,
+    ! its timeStep is equal to the parent timeStep. As a consequence the
+    ! currTime + timeStep is equal to the stopTime of the internal Clock
+    ! for this call of the ModelAdvance() routine.
+    
+    call ESMF_ClockPrint(clock, options="currTime", &
+      preString="------>Advancing ATM from: ", unit=msgString, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call ESMF_LogWrite(msgString, ESMF_LOGMSG_INFO, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    
+    call ESMF_ClockPrint(clock, options="stopTime", &
+      preString="---------------------> to: ", unit=msgString, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call ESMF_LogWrite(msgString, ESMF_LOGMSG_INFO, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+#ifdef NUOPC_TRACE
+    call ESMF_TraceRegionExit("ATM:ModelAdvance")
+#endif
+    
+  end subroutine
+
+end module
