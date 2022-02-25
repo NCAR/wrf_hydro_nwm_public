aa1075ee2110cef4997fc7f04526df2db19f513d
Soren Rasmussen <s.c.rasmussen@gmail.com>
2022-02-24 21:09:28 -0800

    bugfix: solution to fix the request to download google drive file. Has traditionally worked but could not handle the 'file too large for google to scan: continue?' error that was being returned. Future method should use gdown to download

--- tests/local/utils/gdrive_download.py
+++ tests/local/utils/gdrive_download.py
@@ -1,15 +1,21 @@
 from argparse import ArgumentParser
 
 import requests
+# import gdown
 
 
 def download_file_from_google_drive(id, destination):
     print('downloading google drive file id ' + id + ' to ' + destination)
+
+    # --- this will work once gdown is pip installed on docker
+    # gdown.download(id=id, output=destination)
+
     URL = "https://docs.google.com/uc?export=download"
 
     session = requests.Session()
 
-    response = session.get(URL, params={'id': id}, stream=True)
+    response = session.get(URL, params={'id': id, 'alt': 'media', 'confirm':'t'}
+                           , stream=True)
     token = get_confirm_token(response)
 
     if token:
