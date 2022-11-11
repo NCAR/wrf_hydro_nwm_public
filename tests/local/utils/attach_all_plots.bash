bc4059bb958785ef1e4a40fbd697e65a12618736
Andy Gaydos <gaydos@ucar.edu>
2022-11-11 12:29:15 -0700

    mod. Changes GENPARM.TBL for all parameters to create diffs

--- tests/local/utils/attach_all_plots.bash
+++ tests/local/utils/attach_all_plots.bash
@@ -15,6 +15,6 @@ cd $diffs
 for d in `ls -1`
 do
         if [[ -d $d && `ls -1 $d` ]]; then
-            python $cwd/attach_plots_to_pr.py -r andygaydos/wrf_hydro_nwm_public -p $PR -d $d/* -t "$GITHUB_TOKEN"
+            python $cwd/attach_plots_to_pr.py -r andygaydos/wrf_hydro_nwm_public -p $PR -d $d/* 
         fi
 done
