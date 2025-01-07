2682ceee0ef9d4c632b9894d3b5410c738e75f40
Ryan Cabell <rcabell@ucar.edu>
2025-01-07 08:46:16 -0700

    Add initial support for gage-assisted diversions in channel routing (#756)
    * Add initial support for gage-assisted diversions (Type 3) in channel routing
    ---------
    
    Co-authored-by: Soren Rasmussen <s.c.rasmussen@gmail.com>

--- src/Routing/module_channel_routing.F90
+++ src/Routing/module_channel_routing.F90
@@ -509,10 +509,10 @@ use module_RT_data, only: rt_domain  !! JLM: this is only used in a c3 paramter 
       !comment out to prevent excessive file sizes when running model
       !print*, "qloss,dx,WP,WPk,depth,ChannK,qdc,ql,dt,D", qloss,dx,WP,WPk,depth,ChannK,qdc,ql,dt,D
       if((qloss*dt)/D > ((ql*dt)/D - C4)) then
-           qloss = ql - C4*(D/dt)
-           if (qloss < 0) then
-              print*, 'WARNING CHANNEL LOSS IS NEGATIVE',qloss
-           endif
+         qloss = ql - C4*(D/dt)
+         if ((qloss < 0) .and. (ChannK /= 0)) then
+            print*, 'WARNING CHANNEL LOSS IS NEGATIVE',qloss
+         endif
       endif
 
 ! ----------------------------------------------------------------
@@ -1645,7 +1645,7 @@ end subroutine drive_CHANNEL
                                          nudgeWAdvance,                      &
                                          nudge_apply_upstream_muskingumCunge
 #endif
-
+      use module_channel_diversions, only: calculate_diversion
 
        implicit none
 
@@ -1755,6 +1755,10 @@ end subroutine drive_CHANNEL
        integer, allocatable, dimension(:) :: tmpFinalResType
        real, allocatable,dimension(:) :: tmpAssimilatedValue
        character(len=256), allocatable,dimension(:) :: tmpAssimilatedSourceFile
+
+       ! diversions
+       real :: div_src, div_dst
+       character(*), parameter :: free = '(*(g0,1x))'
 
 #ifdef MPP_LAND
        if(my_id .eq. io_id) then
@@ -2004,6 +2008,32 @@ do nt = 1, nsteps
          !              tmpQLINK(k,2) = MUSKINGCUNGE(k,Qup, Quc, QLINK(k,1), &
          !                  QLateral(k), DTRT_CH, So(k),  CHANLEN(k), &
          !                  MannN(k), ChSSlp(k), Bw(k), Tw(k) )
+
+         ! HANDLE DIVERSIONS
+
+         call calculate_diversion(LINKID(k), Quc, div_src, div_dst)
+
+         if (div_src /= 0) then
+            ! remove from upstream
+#ifdef HYDRO_D
+            print free, "DEBUG: diverting", div_src, "of", Quc, "from link id =", LINKID(k) !, "on processor", my_id
+            if (div_src > Quc) &
+               print free, "DEBUG WARNING: diverted flow (", div_src, ") exceeds total flow, zeroing."
+#endif
+            Quc = max(0.0, Quc - div_src)
+            Qup = max(0.0, Qup - div_src)
+         end if
+
+         if (div_dst /= 0) then
+            ! apply observed value to downstream
+#ifdef HYDRO_D
+            print free, "DEBUG: diverting", div_dst, "to link id =", LINKID(k) !, "on processor", my_id
+#endif
+            Qup = div_dst
+            Quc = div_dst
+            tmpQLINK(k,1) = div_dst
+            tmpQLINK(k,2) = div_dst
+         end if
 
 #ifdef WRF_HYDRO_NUDGING
          call nudge_apply_upstream_muskingumCunge( Qup,  Quc,  nudge(k),  k )
