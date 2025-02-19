08b902752d854472fae525bb5b696be76d6bed3c
Soren Rasmussen <s.c.rasmussen@gmail.com>
2025-02-19 11:37:45 -0700

    Fortran NetCDF library searched for changed to netcdff, lib and lib64 directories searched, HINTS for C and Fortran directories split into different variables

--- src/cmake-modules/FindNetCDF.cmake
+++ src/cmake-modules/FindNetCDF.cmake
@@ -34,23 +34,24 @@ find_path (NETCDF_INCLUDES netcdf.h HINTS NETCDF_DIR "$ENV{NETCDF}/include" ENV 
 
 find_path (NETCDF_MODULES netcdf.mod HINTS NETCDF_DIR "$ENV{NETCDF}/mod" ENV NETCDF_MOD ENV MPI_FORTRAN_MOD_DIR "/usr/lib64/gfortran/modules")
 
-find_library(NETCDF_LIBRARIES NAMES netcdf PATHS "$ENV{NETCDF}/lib" ENV NETCDF_LIB ENV MPI_LIB)
+find_library(NETCDF_LIBRARIES NAMES netcdff PATHS "$ENV{NETCDF}" PATH_SUFFIXES lib lib64 ENV NETCDF_LIB ENV MPI_LIB)
 
-find_library (NETCDF_LIBRARIES_C NAMES netcdf PATHS "$ENV{NETCDF}/lib" ENV NETCDF_LIB ENV MPI_LIB)
+find_library (NETCDF_LIBRARIES_C NAMES netcdf PATHS "$ENV{NETCDF}" PATH_SUFFIXES lib lib64 ENV NETCDF_LIB ENV MPI_LIB)
 
 mark_as_advanced(NETCDF_LIBRARIES_C)
 
 set (NetCDF_has_interfaces "YES") # will be set to NO if we're missing any interfaces
 set (NetCDF_libs "${NETCDF_LIBRARIES_C}")
 
-get_filename_component (NetCDF_lib_dirs "${NETCDF_LIBRARIES_C}" PATH)
+get_filename_component(NETCDF_LIB_DIR "${NETCDF_LIBRARIES}" DIRECTORY)
+get_filename_component(NETCDF_LIB_DIR_C "${NETCDF_LIBRARIES_C}" DIRECTORY)
 
 macro (NetCDF_check_interface lang header libs)
   if (NETCDF_${lang})
     find_path (NETCDF_INCLUDES_${lang} NAMES ${header}
       HINTS "${NETCDF_INCLUDES}" "${NETCDF_MODULES}" NO_DEFAULT_PATH)
     find_library (NETCDF_LIBRARIES_${lang} NAMES ${libs}
-      HINTS "${NetCDF_lib_dirs}" NO_DEFAULT_PATH)
+      HINTS "${NETCDF_LIB_DIR}" "${NETCDF_LIB_DIR_C}" NO_DEFAULT_PATH)
     mark_as_advanced (NETCDF_INCLUDES_${lang} NETCDF_LIBRARIES_${lang})
     if (NETCDF_INCLUDES_${lang} AND NETCDF_LIBRARIES_${lang})
       list (INSERT NetCDF_libs 0 ${NETCDF_LIBRARIES_${lang}}) # prepend so that -lnetcdf is last
