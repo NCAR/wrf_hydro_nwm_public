931bc29d9de5b4ae6bd483993f2664692cf3e5bb
Soren Rasmussen <s.c.rasmussen@gmail.com>
2025-08-01 11:49:24 -0600

    Load ml pickle object

--- src/CPL/python_cpl/get_weights.py
+++ src/CPL/python_cpl/get_weights.py
@@ -8,10 +8,33 @@ import joblib
 
 initialized = False
 
+def init_ml_object():
+    dir = '/glade/work/soren/src/wrf-hydro/lanl-ml/'
+    f = dir+"model.pkl"
+    modelpath = f
+
+    try:
+        # Attempt with joblib (most common for sklearn)
+        model = joblib.load(f)
+        print("Loaded using joblib:", type(model))
+    except Exception as e:
+        print("joblib failed, trying pickle...", e)
+        with open(model_path, "rb") as f:
+            model = pickle.load(f)
+            print("Loaded using pickle:", type(model))
+
+    # Inspect model attributes
+    if hasattr(model, "get_params"):
+        print("Model parameters:", model.get_params())
+    else:
+        print("Model does not have get_params (might not be sklearn).")
+
+
 def init_weights():
     global initialized
-    print("Initializing Weights")
+    print("Initializing Python")
     initialized = True
+    init_ml_object()
 
 def get_weights(weights):
     global initialized
