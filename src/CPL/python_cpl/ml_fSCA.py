ca6435468be0c8206eec1f553e297809615cf822
Soren Rasmussen <s.c.rasmussen@gmail.com>
2025-08-08 15:41:40 -0600

    Read slope and aspect from NetCDF files

--- src/CPL/python_cpl/ml_fSCA.py
+++ src/CPL/python_cpl/ml_fSCA.py
@@ -1,13 +1,21 @@
+import joblib
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from xgboost import XGBRegressor, Booster
-import joblib
+import xarray as xr
 
 initialized = False
 model = None
+slope = None
+aspect = None
+lat_s = None
+lat_a = None
+lon_s = None
+lon_a = None
+
 
 def init_ml_object():
     base_dir = '/glade/work/soren/src/wrf-hydro/lanl-ml/'
@@ -66,15 +74,53 @@ def init_ml_object():
 
     return model
 
+def init_aspect():
+    aspect_f = '/glade/campaign/ral/hap/enzminger/snowmodel/topo_vege/domain_UCRB/aspect.nc'
+    ds = xr.open_dataset(aspect_f)
+    return ds["aspect"].values, ds["lat"].values, ds["lon"].values
+
+def init_slope():
+    slope_f = '/glade/campaign/ral/hap/enzminger/snowmodel/topo_vege/domain_UCRB/slope.nc'
+    ds = xr.open_dataset(slope_f)
+    return ds["slope"].values, ds["lat"].values, ds["lon"].values
 
 def init_model():
-    global initialized, model
+    global initialized, model, aspect, slope, lat_a, lat_s, lon_a, lon_s
     print("Initializing XGBoost Model")
     if (not initialized):
         model = init_ml_object()
+        aspect, lat_a, lon_a = init_aspect()
+        slope, lat_s, lon_s = init_slope()
         initialized = True
     return model
 
+def get_aspect(lat, lon, tol=1e-12):
+    # Find the cell where both lat & lon match (within tol)
+    mask = np.isclose(lat_a, lat, atol=tol) & np.isclose(lon_a, lon, atol=tol)
+    hits = np.argwhere(mask)
+
+    if hits.size == 0:
+        raise ValueError("No exact (lat, lon) match found within tolerance.")
+    i, j = map(int, hits[0])        # (south_north, west_east)
+
+    val = aspect[i, j]
+    if np.isnan(val):
+        raise ValueError("Matched cell has NaN aspect.")
+    return val
+
+def get_slope(lat, lon, tol=1e-12):
+    # Find the cell where both lat & lon match (within tol)
+    mask = np.isclose(lat_s, lat, atol=tol) & np.isclose(lon_s, lon, atol=tol)
+    hits = np.argwhere(mask)
+
+    if hits.size == 0:
+        raise ValueError("No exact (lat, lon) match found within tolerance.")
+    i, j = map(int, hits[0])        # (south_north, west_east)
+
+    val = slope[i, j]
+    if np.isnan(val):
+        raise ValueError("Matched cell has NaN aspect.")
+    return val
 
 def ml_fSCA_scalar(T2D, LWDOWN, SWDOWN, U2D, V2D, day_of_year,
                 HGT, slope, aspect, lat, lon):
@@ -88,6 +134,8 @@ def ml_fSCA_scalar(T2D, LWDOWN, SWDOWN, U2D, V2D, day_of_year,
     static_vars = ['HGT', 'slope', "aspect", "lat", "lon"]
     target_var = 'fSCA'
 
+    slope = get_slope(lat, lon)
+    aspect = get_aspect(lat, lon)
 
     # Flatten each variable (column-major to row-major conversion)
     features = np.array([[
@@ -125,6 +173,11 @@ def ml_fSCA_array(T2D, LWDOWN, SWDOWN, U2D, V2D, day_of_year,
     static_vars = ['HGT', 'slope', "aspect", "lat", "lon"]
     target_var = 'fSCA'
 
+
+    # slope = get_slope(lat, lon)
+    # aspect = get_aspect(lat, lon)
+    error_s ="get_slope and get_aspect need to be implemented for arrays"
+    raise ValueError(error_s)
 
     # Flatten each variable (column-major to row-major conversion)
     npoints = nx * ny
